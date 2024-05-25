0f30ae76b1b7215c6993e3ca9315b66ea9dcfe3a
Liam Blair <Liam Blair>
2024-05-25 10:36:10 +0100

    fix: incombatability with sqlalcehmy and pandas with df.to_sql

--- constants.py
+++ constants.py
@@ -4,3 +4,10 @@ import os
 dotenv.load_dotenv()
 
 DOGAPI=os.getenv("DOGAPI")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_TABLE = os.getenv("DB_TABLE")
---
+++ create_db_tbl.py
@@ -0,0 +1,44 @@
+
+import psycopg2
+import constants
+import pandas as pd
+
+# Establish connection to the PostgreSQL database
+conn = psycopg2.connect(
+    dbname=constants.DB_NAME,
+    user=constants.DB_USER,
+    password=constants.DB_PASSWORD,
+    host=constants.DB_HOST,
+    port=constants.DB_PORT
+)
+
+cur = conn.cursor()
+
+# CREATE TABLE SQL command
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS postgres.public.dog_lover_data (
+        id                SERIAL PRIMARY KEY
+        ,name             varchar(255)
+        ,bred_for         varchar(150)
+        ,breed_group      varchar(150)
+        ,weight           varchar(20)
+        ,height           varchar(20)
+        ,life_span        varchar(20)
+        ,temperament      varchar(150)
+        ,origin           varchar(20)
+        ,image            varchar(255)
+        ,weight_min        int
+        ,weight_max        int
+        ,height_min        int
+        ,height_max        int
+        ,life_span_min     int
+        ,life_span_max     int
+    );
+"""
+
+cur.execute(create_table_query)
+
+# Commit and close the transaction
+conn.commit()
+cur.close()
+conn.close()
